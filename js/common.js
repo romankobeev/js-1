a117766ff2f899423528e4b13737a4c9aa2733d1
romank123 <67678390+romank123@users.noreply.github.com>
2022-09-08 20:51:58 +0300

    add vue methods filterGoods

--- js/common.js
+++ js/common.js
@@ -11,41 +11,45 @@ const app = new Vue({
     filtered: [],
     imgCart: "https://via.placeholder.com/50x100",
     product: [],
+    searchLine: "",
     imgProduct: "https://via.placeholder.com/200x500",
   },
   methods: {
-    getJson(url) {
-      return fetch(url)
-        .then((result) => result.json())
-        .catch((error) => console.log(error));
-    },
+    // getJson(url) {
+    //   return fetch(url)
+    //     .then((result) => result.json())
+    //     .catch((error) => console.log(error));
+    // },
 
-    addProduct(item) {
-      this.getJson(`${API}/addBasket.json`).then((data) => {
-        if (data.result === 1) {
-          let find = this.cartItems.find(
-            (el) => el.id_product === item.id_product
-          );
-          if (find) {
-            find.quantity++;
-          } else {
-            const prod = Object.assign({ quantity: 1 }, item);
-            this.cartItems.push(prod);
-          }
-        }
-      });
-    },
+    // addProduct(item) {
+    //   this.getJson(`${API}/addBasket.json`).then((data) => {
+    //     if (data.result === 1) {
+    //       let find = this.cartItems.find(
+    //         (el) => el.id_product === item.id_product
+    //       );
+    //       if (find) {
+    //         find.quantity++;
+    //       } else {
+    //         const prod = Object.assign({ quantity: 1 }, item);
+    //         this.cartItems.push(prod);
+    //       }
+    //     }
+    //   });
+    // },
 
-    remove(item) {
-      this.getJson(`${API}/deleteFromBasket.json`).then((data) => {
-        if (data.resulr === 1) {
-          if (item.quantity > 1) {
-            item.quantity--;
-          } else {
-            this.cartItems.splice(this.cartItems.indexOf(item), 1);
-          }
-        }
-      });
+    // remove(item) {
+    //   this.getJson(`${API}/deleteFromBasket.json`).then((data) => {
+    //     if (data.resulr === 1) {
+    //       if (item.quantity > 1) {
+    //         item.quantity--;
+    //       } else {
+    //         this.cartItems.splice(this.cartItems.indexOf(item), 1);
+    //       }
+    //     }
+    //   });
+    // },
+    filterGoods() {
+      console.log(this.$data.searchLine);
     },
 
     filter() {
@@ -56,23 +60,23 @@ const app = new Vue({
     },
   },
   mounted() {
-    this.getJson(`${API + this.cartUrl}`).then((data) => {
-      for (let item of data.contents) {
-        this.cartItems.push(item);
-      }
-    }),
-      this.getJson(`${API + this.catalogUrl}`).then((data) => {
-        for (let item of data) {
-          this.$data.products.push(item);
-          this.$data.filtered.push(item);
-        }
-      }),
-      this.getJson("getProducts.json").then((data) => {
-        for (let item of data) {
-          this.products.push(item);
-          this.filtered.push(item);
-        }
-      });
+    // this.getJson(`${API + this.cartUrl}`).then((data) => {
+    //   for (let item of data.contents) {
+    //     this.cartItems.push(item);
+    //   }
+    // }),
+    //   this.getJson(`${API + this.catalogUrl}`).then((data) => {
+    //     for (let item of data) {
+    //       this.$data.products.push(item);
+    //       this.$data.filtered.push(item);
+    //     }
+    //   }),
+    //   this.getJson("getProducts.json").then((data) => {
+    //     for (let item of data) {
+    //       this.products.push(item);
+    //       this.filtered.push(item);
+    //     }
+    //   });
   },
 });
 //корзина - пустой массив
