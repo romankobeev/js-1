8451d2b2552dfb0ac5303713c2a52be11f503313
romank123 <67678390+romank123@users.noreply.github.com>
2022-09-12 17:33:27 +0300

    add homework-7

--- js/common.js
+++ js/common.js
@@ -7,6 +7,8 @@ const app = new Vue({
     cartItems: [],
     filtered: [],
     products: [],
+    sum: 0,
+    totalCartItems: 0,
     searchLine: "",
   },
   methods: {
@@ -17,36 +19,68 @@ const app = new Vue({
     },
 
     addToCart(product) {
-      console.log(product);
+      if (this.$data.cartItems.length === 0) {
+        this.$data.cartItems.push(product);
+      }
+
+      if (this.$data.cartItems.find((element) => element.id === product.id)) {
+      } else {
+        this.$data.cartItems.push(product);
+      }
+
+      for (let i = 0; i < this.$data.cartItems.length; i++) {
+        if (this.$data.cartItems[i].id === product.id) {
+          this.$data.cartItems[i].quantity =
+            this.$data.cartItems[i].quantity + 1;
+        }
+      }
+
+      this.$data.sum = 0;
+      for (let i = 0; i < this.$data.cartItems.length; i++) {
+        this.$data.sum += Math.round(
+          this.$data.cartItems[i].price * this.$data.cartItems[i].quantity
+        );
+      }
+      this.totalCartItems = 0;
+      this.$data.cartItems.forEach((element) => {
+        this.totalCartItems += element.quantity;
+      });
     },
 
-    // addProduct(item) {
-    //   this.getJson(`${API}/addBasket.json`).then((data) => {
-    //     if (data.result === 1) {
-    //       let find = this.cartItems.find(
-    //         (el) => el.id_product === item.id_product
-    //       );
-    //       if (find) {
-    //         find.quantity++;
-    //       } else {
-    //         const prod = Object.assign({ quantity: 1 }, item);
-    //         this.cartItems.push(prod);
-    //       }
-    //     }
-    //   });
-    // },
+    removeItem(product) {
+      for (let i = 0; i < this.$data.cartItems.length; i++) {
+        if (
+          this.$data.cartItems[i].id === product.id &&
+          this.$data.cartItems[i].quantity > 0
+        ) {
+          this.$data.cartItems[i].quantity =
+            this.$data.cartItems[i].quantity - 1;
+          this.$data.cartItems[i].id = product.id + 1;
+          this.$data.cartItems[i].id = product.id - 1;
+        }
+        if (this.$data.cartItems[i].quantity === 0) {
+          this.$data.cartItems.splice(i, 1);
+        }
+      }
 
-    // remove(item) {
-    //   this.getJson(`${API}/deleteFromBasket.json`).then((data) => {
-    //     if (data.resulr === 1) {
-    //       if (item.quantity > 1) {
-    //         item.quantity--;
-    //       } else {
-    //         this.cartItems.splice(this.cartItems.indexOf(item), 1);
-    //       }
-    //     }
-    //   });
-    // },
+      this.$data.sum = 0;
+      for (let i = 0; i < this.$data.cartItems.length; i++) {
+        this.$data.sum += Math.round(
+          this.$data.cartItems[i].price * this.$data.cartItems[i].quantity
+        );
+      }
+      this.totalCartItems = 0;
+      this.$data.cartItems.forEach((element) => {
+        this.totalCartItems += element.quantity;
+      });
+    },
+
+    showCarts(event) {
+      if (event.target.hasAttribute("src")) {
+        this.$data.showCart = !this.$data.showCart;
+      }
+    },
+
     filterGoods() {
       this.$data.filtered = [];
       let regexp = new RegExp(this.$data.searchLine, "i");
@@ -56,7 +90,6 @@ const app = new Vue({
           this.$data.filtered.push(item);
         }
       }
-      console.log(this.$data.filtered);
     },
   },
   mounted() {
@@ -64,114 +97,14 @@ const app = new Vue({
       for (let item of data) {
         this.products.push(item);
       }
+      for (let i = 0; i < this.$data.products.length; i++) {
+        this.$data.products[i].quantity = 0;
+      }
       this.$data.filtered = this.$data.products;
     });
   },
 
   watch: {
-    showCart: function () {},
+    cartItems: function (product) {},
   },
 });
-
-/*
-// эта часть кода из курса базовый JS
-//корзина - пустой массив
-const basket = [];
-
-let addToCart = document.querySelectorAll(".img-block__cart-title");
-
-//ловим события по нажатию на кнопку добавить товар в корзину
-addToCart.forEach(function (btn) {
-  btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    let productDetail = event.target.closest(".item-card");
-    //добавляем в корзину текущий товар
-    addToBasket(getProductData(productDetail));
-    //установить в логотипе корзины ко-во товара
-    setCountProduct(getQuantityProduct());
-    //добавить разметку html для списка товаров
-    addToHtml();
-  });
-});
-
-//
-let basketBtn = document.querySelector(".header-block__cart");
-let basketInfo = document.querySelector(".basket-info");
-
-//клик по иконке корзины для отображения списка товаров положенных в корзину
-basketBtn.addEventListener("click", function (event) {
-  basketInfo.classList.toggle("active");
-});
-
-//вывести на странице html с кол-вом товара в корзине
-function setCountProduct(countProduct) {
-  let count = document.querySelector(".header-block__cart-count");
-  count.textContent = countProduct;
-}
-
-//добавить товар в корзину с проверкой есть ли таков уже в корзине
-function addToBasket(obj) {
-  if (basket.length > 0) {
-    let product = basket.find((item) => item.title === obj.title);
-
-    if (product) {
-      //если товар уже есть в корзине
-      let totalPrice = parseFloat(product.price.replace("$", ""));
-      product.quantity = product.quantity + 1;
-      product.totalPrice = Number((totalPrice * product.quantity).toFixed(2));
-    } else {
-      addNewProduct(obj);
-    }
-  } else {
-    addNewProduct(obj);
-  }
-}
-
-//добавить в корзину новый товар, которого нет в корзине
-function addNewProduct(obj) {
-  basket.push(obj);
-}
-
-//Получить данные о товаре(стоимость, цена, кол-во) из html разметки
-function getProductData(productDetail) {
-  let title = productDetail.querySelector("h3").innerText;
-  let price = productDetail.querySelector(".card__price").innerText;
-  let quantity = 1;
-  let totalPrice = Number(parseFloat(price.replace("$", "")));
-  return { title, price, quantity, totalPrice };
-}
-
-//Получить кол-во товаров в корзине
-function getQuantityProduct() {
-  return basket
-    .map((item) => item.quantity)
-    .reduce((prev, curr) => prev + curr, 0);
-}
-
-//Получить общую сумму товаров
-function getTotalAmount() {
-  return basket
-    .map((item) => item.totalPrice)
-    .reduce((prev, curr) => prev + curr)
-    .toFixed(2);
-}
-
-//Добавить html разметку на страницу
-function addToHtml() {
-  let tr = "";
-  let innerHtml = document.querySelector(".basket-info tbody");
-  innerHtml.innerHTML = "";
-  let table = document.querySelector("tbody");
-  let totalAmount = document.querySelector("tfoot th");
-  totalAmount.textContent = "";
-  basket.forEach((product) => {
-    tr += `<tr>
-<td>${product.title}</td>
-<td>${product.quantity} шт</td>
-<td>${product.price}</td>
-<td>${product.totalPrice}$</td>
-</tr>`;
-  });
-  table.insertAdjacentHTML("afterbegin", tr);
-  totalAmount.textContent = `Товаров в корзине на сумму: ${getTotalAmount()}$`;
-} */
